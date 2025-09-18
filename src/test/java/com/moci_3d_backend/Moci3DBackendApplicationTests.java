e653d4b8dca30ff5c6fc813a30312b9919159991
SeongChoel <tldtlddl1234@gmail.com>
2025-09-18 11:31:05 +0900

    init

---
+++ src/test/java/com/moci_3d_backend/Moci3DBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.moci_3d_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Moci3DBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
