7880eee1be251d148b8f7c57df532a2543105405
NewplayerKOR <149351553+NewplayerKOR@users.noreply.github.com>
2025-09-19 15:42:06 +0900

    Feat#17: 공개자료실 관련 DTO생성 및 entity 자료형 수정 (#22)
    * [refactor] : 공개자료실 Entity id값 수정(long -> Long)
    
    * [feat]: 공개자료실 관련 DTO 생성
    
    * [feat]: package명 수정 (User -> user)
    
    * [feat]: 1:N관계 수정

--- src/main/java/com/moci_3d_backend/domain/archive/public_archive/entity/PublicArchive.java
+++ src/main/java/com/moci_3d_backend/domain/archive/public_archive/entity/PublicArchive.java
@@ -1,5 +1,7 @@
 package com.moci_3d_backend.domain.archive.public_archive.entity;
 
+import com.moci_3d_backend.domain.user.entity.User;
+import com.moci_3d_backend.domain.fileUpload.entity.FileUpload;
 import jakarta.persistence.*;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -8,6 +10,8 @@ import org.hibernate.annotations.CreationTimestamp;
 import org.hibernate.annotations.UpdateTimestamp;
 
 import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.List;
 
 @Entity
 @Table(name = "public_archive")
@@ -17,7 +21,7 @@ import java.time.LocalDateTime;
 public class PublicArchive {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private long id;
+    private Long id;
 
     @Column(length = 255, nullable = false)
     private String title;
@@ -32,13 +36,13 @@ public class PublicArchive {
     private String subCategory;
 
     // TODO: mappedBy는 files내 정의된 필드명으로 확인 및 수정할 것
-//    @OneToMany(mappedBy = "publicArchive",  cascade = CascadeType.ALL, orphanRemoval = true) // 글 삭제시 파일도 같이 삭제(고아 삭제)
-//    @JoinColumn(name = "file_id", referencedColumnName = "id", nullable = true) // file_id가 없어도 글 생성 가능
-//    private File file;
-//
-//    @ManyToOne(fetch = FetchType.LAZY)
-//    @JoinColumn(name = "uploaded_by", referencedColumnName = "id", nullable = false)
-//    private User uploadedBy;
+    @OneToMany(mappedBy = "publicArchive",  cascade = CascadeType.ALL, orphanRemoval = true) // 글 삭제시 파일도 같이 삭제(고아 삭제)
+    @JoinColumn(name = "file_id", referencedColumnName = "id", nullable = true) // file_id가 없어도 글 생성 가능
+    private List<FileUpload> fileUploads = new ArrayList<>();
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "uploaded_by", referencedColumnName = "id", nullable = false)
+    private User uploadedBy;
 
     @CreationTimestamp
     @Column(updatable = false)
