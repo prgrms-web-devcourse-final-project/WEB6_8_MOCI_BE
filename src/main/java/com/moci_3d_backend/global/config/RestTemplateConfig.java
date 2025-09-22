21def3729539fbe1322cf3ba6e96900bc4992ccf
SeongChoel <tldtlddl1234@gmail.com>
2025-09-22 14:41:56 +0900

    refactor : 코드 수정

--- src/main/java/com/moci_3d_backend/global/config/RestTemplateConfig.java
+++ src/main/java/com/moci_3d_backend/global/config/RestTemplateConfig.java
@@ -2,12 +2,17 @@ package com.moci_3d_backend.global.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.client.HttpComponentsClientHttpRequestFactory;
 import org.springframework.web.client.RestTemplate;
 
 @Configuration
 public class RestTemplateConfig {
     @Bean
     public RestTemplate restTemplate() {
+        HttpComponentsClientHttpRequestFactory factory = new HttpComponentsClientHttpRequestFactory();
+        factory.setConnectTimeout(10000); // 연결 타임아웃 설정 (10초)
+        factory.setReadTimeout(30000); // 읽기 타임아웃 설정 (30초)
+
         return new RestTemplate();
     }
 }
