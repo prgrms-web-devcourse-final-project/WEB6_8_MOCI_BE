cc049fd5b52dab81ccd8908119a9721347dd7c66
NewplayerKOR <149351553+NewplayerKOR@users.noreply.github.com>
2025-09-22 09:01:50 +0900

    [fix] : Entity 매핑 오류 수정 (#32)

--- src/main/java/com/moci_3d_backend/domain/archive/public_archive/entity/PublicArchive.java
+++ src/main/java/com/moci_3d_backend/domain/archive/public_archive/entity/PublicArchive.java
@@ -1,7 +1,7 @@
 package com.moci_3d_backend.domain.archive.public_archive.entity;
 
-import com.moci_3d_backend.domain.user.entity.User;
 import com.moci_3d_backend.domain.fileUpload.entity.FileUpload;
+import com.moci_3d_backend.domain.user.entity.User;
 import jakarta.persistence.*;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -36,9 +36,8 @@ public class PublicArchive {
     private String subCategory;
 
     // TODO: mappedBy는 files내 정의된 필드명으로 확인 및 수정할 것
-//    @OneToMany(mappedBy = "publicArchive",  cascade = CascadeType.ALL, orphanRemoval = true) // 글 삭제시 파일도 같이 삭제(고아 삭제)
-//    @JoinColumn(name = "file_id", referencedColumnName = "id", nullable = true) // file_id가 없어도 글 생성 가능
-//    private List<FileUpload> fileUploads = new ArrayList<>();
+    @OneToMany(mappedBy = "publicArchive",  cascade = CascadeType.ALL, orphanRemoval = true) // 글 삭제시 파일도 같이 삭제(고아 삭제)
+    private List<FileUpload> fileUploads = new ArrayList<>();
 
     @ManyToOne(fetch = FetchType.LAZY)
     @JoinColumn(name = "uploaded_by", referencedColumnName = "id", nullable = false)
