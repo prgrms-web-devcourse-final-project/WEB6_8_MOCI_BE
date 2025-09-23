8d3c8b1ac9bb3cdaa2f8aafadcb831dc74eb50f2
NewplayerKOR <149351553+NewplayerKOR@users.noreply.github.com>
2025-09-23 11:01:52 +0900

    [refactor]: 기존 Dto 활용

--- src/main/java/com/moci_3d_backend/domain/archive/archive_request/dto/ArchiveRequestResponseDto.java
+++ src/main/java/com/moci_3d_backend/domain/archive/archive_request/dto/ArchiveRequestResponseDto.java
@@ -1,6 +1,7 @@
 package com.moci_3d_backend.domain.archive.archive_request.dto;
 
 import com.moci_3d_backend.domain.archive.archive_request.entity.RequestStatus;
+import com.moci_3d_backend.domain.user.dto.response.UserDetailResponse;
 import lombok.Builder;
 import lombok.Getter;
 
@@ -10,19 +11,11 @@ import java.time.LocalDateTime;
 @Builder
 public class ArchiveRequestResponseDto {
     private final Long id;
-    private final UserDto requester;
+    private final UserDetailResponse requester;
     private final String title;
     private final String description;
     private final RequestStatus status;
-    private final UserDto reviewedBy;
+    private final UserDetailResponse reviewedBy;
     private final LocalDateTime createdAt;
     private final LocalDateTime updatedAt;
-
-    @Getter
-    @Builder
-    public static class UserDto {
-        private final Long id;
-        private final String name;
-        private final String profileImageUrl; // 명세서에 있는 필드
-    }
 }
