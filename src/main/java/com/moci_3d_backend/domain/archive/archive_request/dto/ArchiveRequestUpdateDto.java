b1551a6d147902b53c1bc4c3042cd8368a56ea92
NewplayerKOR <149351553+NewplayerKOR@users.noreply.github.com>
2025-09-23 10:14:10 +0900

    [feat]: codeReview 반영

--- src/main/java/com/moci_3d_backend/domain/archive/archive_request/dto/ArchiveRequestUpdateDto.java
+++ src/main/java/com/moci_3d_backend/domain/archive/archive_request/dto/ArchiveRequestUpdateDto.java
@@ -3,7 +3,7 @@ package com.moci_3d_backend.domain.archive.archive_request.dto;
 import lombok.NoArgsConstructor;
 
 @NoArgsConstructor
-public class ArchiveRequestUpdateDto extends ArchiveRequestCreateDto{
+public class ArchiveRequestUpdateDto extends ArchiveRequestBaseDto{
     public ArchiveRequestUpdateDto(String title, String description) {
         super(title, description);
     }
