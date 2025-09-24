3d50518d08c893a12b42018671a056005677b838
ehrl1225 <wjdwntls1225@naver.com>
2025-09-24 10:45:05 +0900

    [feat] : swagger 문서화

--- src/main/java/com/moci_3d_backend/domain/chat/mentor/mentorChatRoom/controller/ApiV1MenteeChatRoomController.java
+++ src/main/java/com/moci_3d_backend/domain/chat/mentor/mentorChatRoom/controller/ApiV1MenteeChatRoomController.java
@@ -5,6 +5,8 @@ import com.moci_3d_backend.domain.chat.mentor.mentorChatRoom.dto.MentorChatRoomR
 import com.moci_3d_backend.domain.chat.mentor.mentorChatRoom.service.MenteeChatRoomService;
 import com.moci_3d_backend.domain.user.entity.User;
 import com.moci_3d_backend.global.rsData.RsData;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.web.PageableDefault;
@@ -15,10 +17,12 @@ import java.util.List;
 @RestController
 @RequestMapping("/api/v1/chat/mentee/room")
 @RequiredArgsConstructor
+@Tag(name="멘티의 채팅방", description = "멘티의 채팅방 관련 API")
 public class ApiV1MenteeChatRoomController {
     private final MenteeChatRoomService menteeChatRoomService;
 
     @PostMapping()
+    @Operation(summary = "[멘티] 채팅방 생성", description = "멘티가 채팅방을 생성합니다.")
     public RsData<Void> createMentorChatRoom(
             @RequestBody CreateMentorChatRoom createMentorChatRoom,
             User user // 컴파일 에러 방지용
@@ -29,7 +33,8 @@ public class ApiV1MenteeChatRoomController {
     }
 
     @GetMapping()
-    public RsData<List<MentorChatRoomResponse>> getMentorChatRooms(
+    @Operation(summary = "[멘티] 채팅방을 조회합니다.", description = "멘티가 참여한 채팅방을 조회합니다.")
+    public RsData<List<MentorChatRoomResponse>> getMenteeChatRooms(
             @PageableDefault(size = 10, page = 0) Pageable pageable,
             User user // 컴파일 에러 방지용
     ){
@@ -39,7 +44,8 @@ public class ApiV1MenteeChatRoomController {
     }
 
     @DeleteMapping("{room_id}")
-    public RsData<Void> deleteMentorChatRoom(
+    @Operation(summary = "[멘티] 채팅방을 나갑니다.", description = "멘티가 참여한 채팅방을 나갑니다.")
+    public RsData<Void> deleteMenteeChatRoom(
             @PathVariable("room_id") Long roomId,
             User user // 컴파일 에러 방지용
     ){
