e950f872536a3d10bfd4a9ac62a04148ff3574dc
SeongChoel <tldtlddl1234@gmail.com>
2025-09-24 10:30:53 +0900

    [refactor] : 메시지 읽음처리 리팩토링

--- src/main/java/com/moci_3d_backend/domain/chat/ai/aiChatMessage/repository/AiChatMessageRepository.java
+++ src/main/java/com/moci_3d_backend/domain/chat/ai/aiChatMessage/repository/AiChatMessageRepository.java
@@ -1,6 +1,7 @@
 package com.moci_3d_backend.domain.chat.ai.aiChatMessage.repository;
 
 import com.moci_3d_backend.domain.chat.ai.aiChatMessage.entity.AiChatMessage;
+import com.moci_3d_backend.domain.chat.ai.aiChatMessage.enums.MessageStatus;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.List;
@@ -8,4 +9,5 @@ import java.util.List;
 public interface AiChatMessageRepository extends JpaRepository<AiChatMessage, Long> {
 
     List<AiChatMessage> findByRoomIdOrderByIdAsc(Long roomId);
+    List<AiChatMessage> findByRoomIdAndIdLessThanEqualAndStatusNot(Long roomId, Long lastSeenMessageId, MessageStatus messageStatus);
 }
