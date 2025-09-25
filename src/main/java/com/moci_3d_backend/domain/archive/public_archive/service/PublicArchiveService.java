233e6218c21ac5f07060a8adef51245fcfc3911a
NewplayerKOR <149351553+NewplayerKOR@users.noreply.github.com>
2025-09-25 14:08:10 +0900

    [refactor]: 임시 권한 확인 추가

--- src/main/java/com/moci_3d_backend/domain/archive/public_archive/service/PublicArchiveService.java
+++ src/main/java/com/moci_3d_backend/domain/archive/public_archive/service/PublicArchiveService.java
@@ -4,7 +4,6 @@ import com.moci_3d_backend.domain.archive.public_archive.dto.PublicArchiveCreate
 import com.moci_3d_backend.domain.archive.public_archive.dto.PublicArchiveListResponse;
 import com.moci_3d_backend.domain.archive.public_archive.dto.PublicArchiveResponse;
 import com.moci_3d_backend.domain.archive.public_archive.dto.PublicArchiveUpdateRequest;
-import com.moci_3d_backend.domain.archive.public_archive.entity.ArchiveCategory;
 import com.moci_3d_backend.domain.archive.public_archive.entity.PublicArchive;
 import com.moci_3d_backend.domain.archive.public_archive.mapper.PublicArchiveMapper;
 import com.moci_3d_backend.domain.archive.public_archive.repository.PublicArchiveRepository;
@@ -36,9 +35,12 @@ public class PublicArchiveService {
     // 교육 자료실 게시물 생성
     @Transactional
     public PublicArchiveResponse createPublicArchive(PublicArchiveCreateRequest request, Long userId) {
-
+        // 권한 확인 (관리자)
         User user = userRepository.findById(userId)
-                .orElseThrow(() -> new EntityNotFoundException("해당 ID의 사용자를 찾을 수 없습니다." + userId));
+                        .orElseThrow(() -> new EntityNotFoundException("해당 ID의 사용자를 찾을 수 없습니다." + userId));
+                if (!User.UserRole.ADMIN.equals(user.getRole())) {
+                    throw new IllegalStateException("관리자 권한이 필요합니다.");
+                }
 
         PublicArchive newArchive = new PublicArchive();
         newArchive.setTitle(request.getTitle());
@@ -75,7 +77,13 @@ public class PublicArchiveService {
 
     // 교육 자료실 게시물 수정
     @Transactional
-    public PublicArchiveResponse updatePublicArchive(Long archiveId, PublicArchiveUpdateRequest request) {
+    public PublicArchiveResponse updatePublicArchive(Long archiveId, PublicArchiveUpdateRequest request, Long userId) {
+        User user = userRepository.findById(userId)
+                .orElseThrow(() -> new EntityNotFoundException("해당 ID의 사용자를 찾을 수 없습니다." + userId));
+        if (!User.UserRole.ADMIN.equals(user.getRole())) {
+            throw new IllegalStateException("관리자 권한이 필요합니다.");
+        }
+
         PublicArchive existingArchive = publicArchiveRepository.findById(archiveId)
                 .orElseThrow(() -> new EntityNotFoundException("해당 ID의 교육 자료실 글을 찾을 수 없습니다: " + archiveId));
         if (request.getTitle() != null) existingArchive.setTitle(request.getTitle());
@@ -88,7 +96,13 @@ public class PublicArchiveService {
 
     // 교육 자료실 게시물 삭제
     @Transactional
-    public void deletePublicArchive(Long archiveId) {
+    public void deletePublicArchive(Long archiveId, Long userId) {
+        User user = userRepository.findById(userId)
+                .orElseThrow(() -> new EntityNotFoundException("해당 ID의 사용자를 찾을 수 없습니다." + userId));
+        if (!User.UserRole.ADMIN.equals(user.getRole())) {
+            throw new IllegalStateException("관리자 권한이 필요합니다.");
+        }
+
         if (!publicArchiveRepository.existsById(archiveId)) {
             throw new EntityNotFoundException("해당 ID의 교육 자료실 글을 찾을 수 없습니다: " + archiveId);
         }
