58ea1e3820afcbaf52f1b60844a499e5d603744b
MadeByPark <dudwlsdl4817@gmail.com>
2025-09-28 11:52:11 +0900

    토큰 발급 테스트 케이스 추가

--- src/main/java/com/moci_3d_backend/global/initData/TestInitData.java
+++ src/main/java/com/moci_3d_backend/global/initData/TestInitData.java
@@ -8,6 +8,7 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Lazy;
 import org.springframework.context.annotation.Profile;
 
+// 이 파일은 삭제해도 괜찮을것 같습니다.
 @Configuration
 @Profile("test")
 @RequiredArgsConstructor
---
+++ src/test/java/com/moci_3d_backend/domain/user/controller/AuthControllerTest.java
@@ -0,0 +1,78 @@
+package com.moci_3d_backend.domain.user.controller;
+
+import com.moci_3d_backend.domain.user.entity.User;
+import com.moci_3d_backend.domain.user.service.UserService;
+import jakarta.servlet.http.Cookie;
+import org.hamcrest.Matchers;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.MediaType;
+import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.ResultActions;
+import org.springframework.transaction.annotation.Transactional;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
+
+@ActiveProfiles("test")
+@SpringBootTest
+@AutoConfigureMockMvc
+@Transactional
+public class AuthControllerTest {
+    @Autowired
+    private UserService userService;
+    @Autowired
+    private MockMvc mvc;
+
+    @Test
+    @DisplayName("로그인(토큰 발급) - 성공")
+    void t2() throws Exception {
+        ResultActions resultActions = mvc
+                .perform(
+                        post("/api/v1/auth/token")
+                                .contentType(MediaType.APPLICATION_JSON)
+                                .content("""
+                                        {
+                                            "loginType": "PHONE",
+                                            "userId": "01012345678",
+                                            "password": "admin123"
+                                        }
+                                        """.stripIndent())
+                )
+                .andDo(print());
+
+        User user = userService.findByUserId("01012345678");
+
+        resultActions
+                .andExpect(handler().handlerType(AuthController.class))
+                .andExpect(handler().methodName("token"))
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$.code").value("200"))
+                .andExpect(jsonPath("$.message").value("success"))
+                .andExpect(jsonPath("$.data").exists())
+                .andExpect(jsonPath("$.data.user").exists())
+                .andExpect(jsonPath("$.data.user.id").value(user.getId()))
+                .andExpect(jsonPath("$.data.user.userId").value(user.getUserId()))
+                .andExpect(jsonPath("$.data.user.refreshToken").doesNotExist()) 
+                .andExpect(jsonPath("$.data.user.name").value(user.getName()))
+                .andExpect(jsonPath("$.data.user.email").value(user.getEmail()))
+                .andExpect(jsonPath("$.data.user.role").value(user.getRole().name()))
+                .andExpect(jsonPath("$.data.user.digitalLevel").value(user.getDigitalLevel()))
+                .andExpect(jsonPath("$.data.user.createdAt").value(Matchers.startsWith(user.getCreatedAt().toString().substring(0, 20))));
+
+        resultActions.andExpect(
+                result -> {
+                    Cookie apiKeyCookie = result.getResponse().getCookie("refreshToken");
+                    assertThat(apiKeyCookie.getValue()).isEqualTo(user.getRefreshToken());
+                    assertThat(apiKeyCookie.getPath()).isEqualTo("/");
+                    assertThat(apiKeyCookie.getAttribute("HttpOnly")).isEqualTo("true");
+                }
+        );
+    }
+}
