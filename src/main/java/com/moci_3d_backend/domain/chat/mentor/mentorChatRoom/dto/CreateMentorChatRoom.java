7b657a1d961a2a1285377b367edf8edb80cd07b5
ehrl1225 <wjdwntls1225@naver.com>
2025-10-02 15:18:48 +0900

    [feat] : dto validation 추가

--- src/main/java/com/moci_3d_backend/domain/chat/mentor/mentorChatRoom/dto/CreateMentorChatRoom.java
+++ src/main/java/com/moci_3d_backend/domain/chat/mentor/mentorChatRoom/dto/CreateMentorChatRoom.java
@@ -1,10 +1,13 @@
 package com.moci_3d_backend.domain.chat.mentor.mentorChatRoom.dto;
 
+import jakarta.validation.constraints.NotBlank;
 import lombok.Getter;
 
 @Getter
 public class CreateMentorChatRoom {
+    @NotBlank
     private String category;
+    @NotBlank
     private String question;
 
     public CreateMentorChatRoom(String category, String question) {
