52bd17ba2e14ad3d2bdfc112ab8a8a2642d1c831
SeongChoel <tldtlddl1234@gmail.com>
2025-10-04 19:41:34 +0900

    [hotfix] : setCookie 함수 수정 (테스트)5

--- src/main/java/com/moci_3d_backend/global/springDoc/SpringDocConfig.java
+++ src/main/java/com/moci_3d_backend/global/springDoc/SpringDocConfig.java
@@ -4,12 +4,22 @@ import io.swagger.v3.oas.annotations.OpenAPIDefinition;
 import io.swagger.v3.oas.annotations.enums.SecuritySchemeType;
 import io.swagger.v3.oas.annotations.info.Info;
 import io.swagger.v3.oas.annotations.security.SecurityScheme;
+import io.swagger.v3.oas.annotations.servers.Server;
 import org.springdoc.core.models.GroupedOpenApi;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
-@OpenAPIDefinition(info = @Info(title = "API 서버", version = "beta", description = "API 서버 문서입니다."))
+@OpenAPIDefinition(
+        info = @Info(
+                title = "API 서버",
+                version = "beta",
+                description = "API 서버 문서입니다."
+        ),
+        servers = {
+                @Server(url = "https://api.mydidimdol.com", description = "Production Server")
+        }
+)
 @SecurityScheme(
         name = "bearerAuth",
         type = SecuritySchemeType.HTTP,
