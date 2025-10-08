f992440329bf71252ed9daf09f4e10c2ba36430d
SeongChoel <tldtlddl1234@gmail.com>
2025-10-08 16:34:43 +0900

    [refactor] : 코드 수정

--- src/main/java/com/moci_3d_backend/domain/chat/ai/aiChatMessage/controller/AiChatStreamController.java
+++ src/main/java/com/moci_3d_backend/domain/chat/ai/aiChatMessage/controller/AiChatStreamController.java
@@ -15,7 +15,7 @@ import org.springframework.web.bind.annotation.*;
 import reactor.core.publisher.Flux;
 
 @RestController
-@RequestMapping("api/v1/chat/ai/rooms")
+@RequestMapping("/api/v1/chat/ai/rooms")
 @Validated
 @Tag(name = "AiChatStreamController", description = "AI 메시지 관리 엔드포인트2(스트리밍)")
 @RequiredArgsConstructor
@@ -32,13 +32,13 @@ public class AiChatStreamController {
     @Operation(summary = "사람이 메시지 보내고, AI 응답까지 한 번에 받기(비동기)",
             description = """
                     사람이 메시지를 보내고, AI의 응답을 스트리밍으로 받습니다.
-                    1. 프론트에서는 roomId를 Path로 넘기고 content만 쿼리파라미터에 담아보냅니다.
+                    1. 프론트에서는 roomId를 Path로 넘기고 content만 바디에 담아보냅니다.
                     2. AI의 응답을 스트리밍 시작
                     3. 프론트는 이벤트 받을 때마다 화면에 바로바로 출력
                     4. 스트리밍 종료후, 서버쪽에서 최종 AI 메세지를 DB에 저장
                     스트리밍 응답은 계속 이어지는 텍스트 스트림이지 완성된 JSON 객체가 아님 
                     """)
-    @PostMapping(value = "{roomId}/ask-stream")
+    @PostMapping(value = "/{roomId}/ask-stream")
     public Flux<ServerSentEvent<String>> askAiStream(@PathVariable Long roomId,
                                                      @RequestBody AiStreamChatRequest req) {
         User actor = rq.getActor();
