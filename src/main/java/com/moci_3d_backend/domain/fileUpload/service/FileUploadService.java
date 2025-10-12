1c1544c8e1d500e767f4275bb6a1fbd98eededa5
SoonTaeYouL <smile971021@naver.com>
2025-10-12 18:44:57 +0900

    [Fix]: file 미리보기수정

--- src/main/java/com/moci_3d_backend/domain/fileUpload/service/FileUploadService.java
+++ src/main/java/com/moci_3d_backend/domain/fileUpload/service/FileUploadService.java
@@ -11,6 +11,7 @@ import org.springframework.web.multipart.MultipartFile;
 import software.amazon.awssdk.core.sync.RequestBody;
 import software.amazon.awssdk.services.s3.S3Client;
 import software.amazon.awssdk.services.s3.model.DeleteObjectRequest;
+import software.amazon.awssdk.services.s3.model.ObjectCannedACL;
 import software.amazon.awssdk.services.s3.model.PutObjectRequest;
 
 import java.io.File;
@@ -47,12 +48,31 @@ public class FileUploadService {
             String fileUrl = "https://" + bucketName + ".s3.ap-northeast-2.amazonaws.com/" + saveName;
 
             log.info("✅ S3 업로드 완료: {}", fileUrl);
+
+            // local에 임시 저장
+            File uploadDir = new File(filePath).getAbsoluteFile();
+            if (!uploadDir.exists()) {
+                boolean created = uploadDir.mkdirs();
+                if (created) {
+                    log.info("업로드 디렉토리 생성: {}", uploadDir.getAbsolutePath());
+                } else if (!uploadDir.exists()) {
+                    log.error("업로드 디렉토리 생성 실패: {}", uploadDir.getAbsolutePath());
+                    throw new RuntimeException("업로드 디렉토리 생성 실패: " + uploadDir.getAbsolutePath());
+                }
+            }
+
+            File localFile = new File(uploadDir, saveName);
+            file.transferTo(localFile);
+            log.info("파일 저장 완료: {}", localFile.getCanonicalPath());
+
             // DB 저장
             FileUpload fileUpload = new FileUpload(
                     file.getOriginalFilename(),
                     saveName,
                     file.getContentType()
             );
+
+
             return FileUploadDto.from(fileUploadRepository.save(fileUpload));
         } catch (IOException e) {
             log.error("❌ 파일 업로드 실패: {}", e.getMessage(), e);
